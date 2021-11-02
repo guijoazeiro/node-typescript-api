4c003d559991bbffae8d702b042ebed371d53391
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-11-02 16:07:54 -0300

    Finish first test case

---
+++ src/clients/_test_/stormGlass.test.ts
@@ -0,0 +1,17 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormglassWeather3Hours from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+    axios.get = jest.fn().mockResolvedValue({ data: stormglassWeather3Hours });
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormglassNormalizedResponseFixture);
+  });
+});
