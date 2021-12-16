25560bf94d30236292cec43a0df446d3c2b4a517
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2021-12-16 15:28:53 -0300

    Add empty beach array test

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -85,4 +85,10 @@ describe('Forecast Service', () => {
     const beachesWithRating = await forecast.processForecastForBeaches(beaches);
     expect(beachesWithRating).toEqual(expectedResponse);
   });
+
+  it('should return an empty list when the beaches array is empty', async () => {
+    const forecast = new Forecast();
+    const response = await forecast.processForecastForBeaches([]);
+    expect(response).toEqual([]);
+  });
 });
