4175e1af1f84203728771abae5660a8660edb9ff
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-10 17:29:40 -0300

    Fix Beach interfaces

--- src/services/__test__/forecast.test.ts
+++ src/services/__test__/forecast.test.ts
@@ -1,6 +1,7 @@
 import { StormGlass } from '@src/clients/stormGlass';
+import { Beach, BeachPosition } from '@src/models/beach';
 import stormGlassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
-import { Beach, BeachPosition, Forecast, ForecastProcessingInternalError } from '../forecast';
+import { Forecast, ForecastProcessingInternalError } from '../forecast';
 
 jest.mock('@src/clients/stormGlass');
 
@@ -15,8 +16,7 @@ describe('Forecast Service', () => {
         lat: -33.792726,
         lng: 151.289824,
         name: 'Manly',
-        position: BeachPosition.E,
-        user: 'some-id',
+        position: BeachPosition.E,        
       },
     ];
     const expectedResponse = [
@@ -98,8 +98,7 @@ describe('Forecast Service', () => {
         lat: -33.792726,
         lng: 151.289824,
         name: 'Manly',
-        position: BeachPosition.E,
-        user: 'some-id',
+        position: BeachPosition.E,        
       },
     ];
     mockedStormGlassService.fetchPoints.mockRejectedValue(
