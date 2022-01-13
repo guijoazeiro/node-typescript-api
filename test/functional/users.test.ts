1438245185c34de319840fc13923d0937a002b98
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-13 19:49:08 -0300

    Add create user with hashed password test

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,11 +1,11 @@
-import { User } from '@src/models/user';
+import { User, comparePassword } from '@src/models/user';
 
 describe('Users functional test', () => {
   beforeEach(async () => {
     await User.deleteMany({});
   });
   describe('When creating a new user', () => {
-    it('should successfully create a new user', async () => {
+    it('should successfully create a new user with encrypted password', async () => {
       const newUser = {
         name: 'John Doe',
         email: 'john@mail.com',
@@ -14,7 +14,15 @@ describe('Users functional test', () => {
 
       const response = await global.testRequest.post('/users').send(newUser);
       expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+      await expect(
+        comparePassword(newUser.password, response.body.password)
+      ).resolves.toBeTruthy();
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) },
+        })
+      );
     });
 
     it('Should return 422 when there is a validation error', async () => {
