9fc169a671bd640e9a13dfcb68bce0bdc35e1e82
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-14 15:43:33 -0300

    Add authenticate route

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -2,6 +2,7 @@ import { Controller, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from './index';
+import AuthService from '@src/services/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -14,5 +15,18 @@ export class UsersController extends BaseController {
     } catch (error) {
       this.sendCreateUpdateErrorResponse(res, error);
     }
+  }
+  @Post('authenticate')
+  public async authenticate(req: Request, res: Response): Promise<void> {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return;
+    }
+    if (!(await AuthService.comparePasswords(password, user.password))) {
+      return;
+    }
+    const token = AuthService.generateToken(user.toJSON());
+    res.status(200).send({ token: token });
   }
 }
