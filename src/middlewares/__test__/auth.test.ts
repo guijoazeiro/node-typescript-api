2859e59fac8250cec695ad93a4ca7838e653a2d5
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-16 20:10:32 -0300

    Add unauthorized token error test

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -14,4 +14,25 @@ describe('Auth middleware', () => {
     authMiddleware(reqFake, resFake, nextFake);
     expect(nextFake).toHaveBeenCalled();
   });
+  
+  it('should return UNAUTHORIZED if there is a problem on the token verification', () => {
+    const reqFake = {
+      headers: {
+        'x-access-token': 'invalid token',
+      },
+    };
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock,
+      })),
+    };
+    const nextFake = jest.fn();
+    authMiddleware(reqFake, resFake as object, nextFake);
+    expect(resFake.status).toHaveBeenCalledWith(401);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      error: 'jwt malformed',
+    });
+  });
 });
