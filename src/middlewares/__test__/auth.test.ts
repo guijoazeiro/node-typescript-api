c3bfecb7691e5ed169102200728073f3264ee7fb
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-17 16:36:42 -0300

    Add no token auth test

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -35,4 +35,23 @@ describe('Auth middleware', () => {
       error: 'jwt malformed',
     });
   });
+
+    it('should return ANAUTHORIZED middleware if theres no token', () => {
+      const reqFake = {
+        headers: {},
+      };
+      const sendMock = jest.fn();
+      const resFake = {
+        status: jest.fn(() => ({
+          send: sendMock,
+        })),
+      };
+      const nextFake = jest.fn();
+      authMiddleware(reqFake, resFake as object, nextFake);
+      expect(resFake.status).toHaveBeenCalledWith(401);
+      expect(sendMock).toHaveBeenCalledWith({
+        code: 401,
+        error: 'jwt must be provided',
+      });
+    });  
 });
