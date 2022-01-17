6810e273a61c4840a7176835ecf3c4433d05d7d9
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-17 17:42:34 -0300

    Prettier Style Fix

--- src/middlewares/__test__/auth.test.ts
+++ src/middlewares/__test__/auth.test.ts
@@ -36,22 +36,22 @@ describe('Auth middleware', () => {
     });
   });
 
-    it('should return ANAUTHORIZED middleware if theres no token', () => {
-      const reqFake = {
-        headers: {},
-      };
-      const sendMock = jest.fn();
-      const resFake = {
-        status: jest.fn(() => ({
-          send: sendMock,
-        })),
-      };
-      const nextFake = jest.fn();
-      authMiddleware(reqFake, resFake as object, nextFake);
-      expect(resFake.status).toHaveBeenCalledWith(401);
-      expect(sendMock).toHaveBeenCalledWith({
-        code: 401,
-        error: 'jwt must be provided',
-      });
-    });  
+  it('should return ANAUTHORIZED middleware if theres no token', () => {
+    const reqFake = {
+      headers: {},
+    };
+    const sendMock = jest.fn();
+    const resFake = {
+      status: jest.fn(() => ({
+        send: sendMock,
+      })),
+    };
+    const nextFake = jest.fn();
+    authMiddleware(reqFake, resFake as object, nextFake);
+    expect(resFake.status).toHaveBeenCalledWith(401);
+    expect(sendMock).toHaveBeenCalledWith({
+      code: 401,
+      error: 'jwt must be provided',
+    });
+  });
 });
