1c3ebc2fa0cb1e00ccda54221aebbc7f84b08280
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-20 13:12:19 -0300

    Add pinopretty

--- src/server.ts
+++ src/server.ts
@@ -49,7 +49,7 @@ export class SetupServer extends Server {
 
   public start(): void {
     this.app.listen(this.port, () => {
-      logger.info('Server listening of port: ', this.port);      
+      logger.info('Server listening of port: ', +this.port);
     });
   }
 }
