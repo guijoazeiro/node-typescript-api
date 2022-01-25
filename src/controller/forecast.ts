be7013590cf50437b29eec1ac3ec746eafe1f96f
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-25 16:34:47 -0300

    Add APIError to forecast controller

--- src/controller/forecast.ts
+++ src/controller/forecast.ts
@@ -1,15 +1,15 @@
 import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
-import logger from '@src/logger';
 import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
 import { Request, Response } from 'express';
+import { BaseController } from '.';
 
 const forecast = new Forecast();
 
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
-export class ForecastController {
+export class ForecastController extends BaseController {
   @Get('')
   public async getForecastForLoggedUser(
     req: Request,
@@ -20,8 +20,10 @@ export class ForecastController {
       const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
     } catch (error) {
-      logger.error(error);
-      res.status(500).send({ error: 'Something went wrong' });
+      this.sendErrorResponse(res, {
+        code: 500,
+        message: 'Something went wrong',
+      });
     }
   }
 }
