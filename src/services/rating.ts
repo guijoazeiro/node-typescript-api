7e1492ebefca6d71977d172a026101d79df0076e
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-26 16:22:13 -0300

    Add rating

---
+++ src/services/rating.ts
@@ -0,0 +1,14 @@
+import { BeachPosition } from '@src/models/beach';
+
+export class Rating {
+ 
+  public getRatingBasedOnWindAndWavePositions(
+    waveDirection: BeachPosition,
+    windDirection: BeachPosition
+  ): number {
+    if (waveDirection === windDirection) {
+      return 1;
+    }
+    return 0;
+  }
+}
