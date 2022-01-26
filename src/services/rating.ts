d786ca824a560fda0f2758e1edcee0620d6935b2
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-26 16:40:56 -0300

    Add isWindOffShore function

--- src/services/rating.ts
+++ src/services/rating.ts
@@ -1,13 +1,36 @@
-import { BeachPosition } from '@src/models/beach';
+import { Beach, BeachPosition } from '@src/models/beach';
 
 export class Rating {
+    constructor(private beach: Beach){
+
+    }
+
   public getRatingBasedOnWindAndWavePositions(
     waveDirection: BeachPosition,
     windDirection: BeachPosition
   ): number {
     if (waveDirection === windDirection) {
       return 1;
+    } else if (this.isWindOffShore(waveDirection, windDirection)){
+        return 5;
     }
     return 3;
+  }
+
+  private isWindOffShore(waveDirection: BeachPosition, windDirection: BeachPosition): boolean{
+    return (
+        (waveDirection === BeachPosition.N &&
+          windDirection === BeachPosition.S &&
+          this.beach.position === BeachPosition.N) ||
+        (waveDirection === BeachPosition.S &&
+          windDirection === BeachPosition.N &&
+          this.beach.position === BeachPosition.S) ||
+        (waveDirection === BeachPosition.E &&
+          windDirection === BeachPosition.W &&
+          this.beach.position === BeachPosition.E) ||
+        (waveDirection === BeachPosition.W &&
+          windDirection === BeachPosition.E &&
+          this.beach.position === BeachPosition.W)
+      );
   }
 }
