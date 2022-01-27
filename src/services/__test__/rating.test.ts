00b14572d446b138dfebe319af28e8cfc595d086
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-27 15:24:00 -0300

    Add calculate rating for point test

--- src/services/__test__/rating.test.ts
+++ src/services/__test__/rating.test.ts
@@ -12,7 +12,23 @@ describe('Rating Service', () => {
 
   const defaultRating = new Rating(defaultBeach);
 
-  describe('Calculate rating for a given point', () => {});
+  describe('Calculate rating for a given point', () => {
+    const defaultPoint = {
+      swellDirection: 110,
+      swellHeight: 0.1,
+      swellPeriod: 5,
+      time: 'test',
+      waveDirection: 110,
+      waveHeight: 0.1,
+      windDirection: 100,
+      windSpeed: 100,
+    };
+
+    it('should get a rating less than 1 for a poor point', () =>{
+      const rating = defaultRating.getRateForPoint(defaultPoint);
+      expect(rating).toBe(1);
+    })
+  });
 
   describe('Get rating based on wind ande wave position', () => {
     it('Should get rating 1 for a beach with onshore winds', () => {
