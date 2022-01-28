cfc1494e6a02e3f56a5e68b67276ef6f217d6eb1
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-01-28 13:21:45 -0300

    Refactor beaches error validation test

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -49,7 +49,7 @@ describe('Beaches functional tests', () => {
         code: 400,
         error: 'Bad Request',
         message:
-          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+          'request.body.lat should be number',
       });
     });
   });
